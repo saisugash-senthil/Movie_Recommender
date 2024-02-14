63efb038d0ec57cdf0bcf8896731678887922411
Saisugash Senthil <128836949+saisugash-senthil@users.noreply.github.com>
2024-02-14 14:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,13 @@ def recommend(movie):
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 file_name = "movie_dict.pkl"
-# file_path = os.path.join(script_directory, file_name)
-with open(file_name 'rb') as file:
+file_path = os.path.join(script_directory, file_name)
+with open(file_path,'rb') as file:
     movies_dict = pickle.load(file)
 movies = pd.DataFrame(movies_dict)
 file_2 = "similarity.pkl"
-similarity_path = os.path.join(script_directory, file_2)
-with open(similarity_path,'rb') as file:
+# similarity_path = os.path.join(script_directory, file_2)
+with open(file_2,'rb') as file:
     similarity = pickle.load(file)
 
 st.title('Movie Recommender System')
